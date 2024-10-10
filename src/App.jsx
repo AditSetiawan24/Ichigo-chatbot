87908c61d2d2a44852f7baf46c7d22b3d0cb5fcd
Adit Setiawan <147960495+AditSetiawan24@users.noreply.github.com>
2024-10-10 22:24:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -158,19 +158,15 @@ function App() {
           >
             {/* Paper Airplane Icon */}
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 3l18 9-9 9-3-3 3-3-9-9z"
-              />
-            </svg>
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-6 h-6 text-white"
+    fill="none"
+    viewBox="2 2 20 20"
+    stroke="currentColor"
+    strokeWidth="1"
+  >
+    <path d="M10 14L21 3m0 0l-6.5 18a.55.55 0 0 1-1 0L10 14l-7-3.5a.55.55 0 0 1 0-1z" />
+  </svg>
           </button>
         </div>
       </div>
