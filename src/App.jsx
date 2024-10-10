5a9c7ebab702d493a6f8d3c2f3a9948d71514b18
Adit Setiawan <147960495+AditSetiawan24@users.noreply.github.com>
2024-10-10 22:49:26 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -172,7 +172,7 @@ function App() {
       </div>
 
 {/*       <footer className="fixed bottom-0 left-1/2 transform -translate-x-1/2 p-2 sm:hidden"> */}
-        <div className="container mx-auto text-center fixed bottom-0 left-1/2 transform -translate-x-1/2 p-2 mt-5">
+        <div className="container mx-auto text-center fixed bottom-0 left-1/2 transform -translate-x-1/2 p-2 mt-20">
           <p className="text-lg">Creator :</p>
           <div className="flex justify-center mt-4 space-x-6">
             <a href="https://github.com/AditSetiawan24" target="_blank" className="hover:text-gray-400">
